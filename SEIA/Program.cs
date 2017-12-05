4c544b4129020837ba1fb804c5e59ae87dfb05d8
eliseu10 <eliseo7@live.com.pt>
2017-12-05 11:34:07 +0000

    Verificação inicial do módulo SEIA

---
+++ SEIA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Npgsql;
+
+namespace SEIA
+{
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			/*
+			var connString = ;
+			var conn = new NpgsqlConnection (connString);
+			conn.Open ();
+			var cmd = new NpgsqlCommand ("SELECT count(*) FROM equipments", conn);
+			Int64 count = (Int64)cmd.ExecuteScalar();
+
+			Console.Write("{0}\n", count);
+			conn.Close();*/
+
+
+			DataBase db = new DataBase ();
+			Int64 age = db.SearchAge (1);
+			Console.WriteLine ("Idade: " + age);
+
+			Int64 nFlaws = db.SearchNumberFlaws(1);
+			Console.WriteLine ("N Relatorios: " + nFlaws);
+
+			Int64 nMaxFlaws = db.SearchMaxNumberFlaws();
+			Console.WriteLine ("N Maximo Relatorios: " + nMaxFlaws);
+
+			//Console.WriteLine (age.ToString() );
+
+		}
+	}
+}
